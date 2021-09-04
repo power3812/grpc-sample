91e062889cfef357bb74613c3bdf14f03281933c
Yuta Nakashima <33017398+power3812@users.noreply.github.com>
2021-09-04 19:20:45 +0900

    clientを追加

---
+++ client/src/App.svelte
@@ -0,0 +1,49 @@
+<script>
+  import { GreeterPromiseClient } from "./helloworld/proto/helloworld_grpc_web_pb";
+  import { HelloRequest } from "./helloworld/proto/helloworld_pb";
+
+  export let name;
+  let res = "";
+  const client = new GreeterPromiseClient("http://localhost:8080", null, null);
+
+  const handlerClick = () => {
+    console.log("click!!!!!!!!!!");
+    const request = new HelloRequest();
+    request.setName("worlddddddddddddd");
+    client.sayHello(request).then((reply) => {
+      res = reply.getMessage();
+    });
+  };
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>{res}</p>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+  <button on:click={handlerClick}>クリック</button>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
