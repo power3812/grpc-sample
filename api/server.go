cff82ef0bc35ee0b313db4c9a094df341e025b07
Yuta Nakashima <33017398+power3812@users.noreply.github.com>
2021-09-04 19:19:33 +0900

    apiを追加

---
+++ api/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	pb "grpc-sample/helloworld/proto"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type HelloworldHandler struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (h HelloworldHandler) SayHello(ctx context.Context, request *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Hello " + request.Name}, nil
+}
+
+func (h HelloworldHandler) SayRepeatHello(request *pb.RepeatHelloRequest, server pb.Greeter_SayRepeatHelloServer) error {
+	panic("implement me")
+}
+
+func main() {
+	port := ":9090"
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal("failed to listen %v", err)
+	}
+	server := grpc.NewServer()
+	pb.RegisterGreeterServer(server, &HelloworldHandler{})
+	reflection.Register(server)
+
+	log.Printf("start gPRC server")
+	server.Serve(lis)
+}
